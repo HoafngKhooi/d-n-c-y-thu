412107468d88eb13890bb3e184902f8dd22baffc
HoafngKhooi <hoafngkhooi@gmail.com>
2026-04-09 22:23:28 +0700

    Update load.lua

--- load.lua
+++ load.lua
@@ -2,6 +2,6 @@
 print("--- Đang tải AI tự học... ---")
 
 -- Thay cái link bên dưới bằng link RAW bạn vừa copy ở B
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HoafngKhooi/d-n-c-y-thu/refs/heads/main/AI_Ten_La_Dan/Bridge_Gateway.py"))()
+loadstring(game:HttpGet("https://www.google.com/search?q=https://raw.githubusercontent.com/HoafngKhooi/d-n-c-y-thu/refs/heads/main/AI_Ten_La_Dan/Game_Observer.lua"))()
 
 print("--- AI đã sẵn sàng! Kiểm tra Terminal trên GitHub để thấy dữ liệu. ---")
