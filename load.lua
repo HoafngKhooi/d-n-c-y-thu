897ad59dff5c4a1fd053cd64a822c9bd3644ecf3
HoafngKhooi <hoafngkhooi@gmail.com>
2026-04-24 01:40:36 +0700

    Delete load.lua

--- load.lua
+++
@@ -1,7 +0,0 @@
--- Script Loading cho AI Blade Ball
-print("--- Đang tải AI tự học... ---")
-
--- Thay cái link bên dưới bằng link RAW bạn vừa copy ở B
-loadstring(game:HttpGet("https://www.google.com/search?q=https://raw.githubusercontent.com/HoafngKhooi/d-n-c-y-thu/refs/heads/main/AI_Ten_La_Dan/Game_Observer.lua"))()
-
-print("--- AI đã sẵn sàng! Kiểm tra Terminal trên GitHub để thấy dữ liệu. ---")
