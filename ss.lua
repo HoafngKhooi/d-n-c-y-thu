660ecdce6e4dab2ecaa4972416e523b91e4a54b9
HoafngKhooi <hoafngkhooi@gmail.com>
2026-06-26 01:10:53 +0700

    Update ss.lua to include commented loadstring

--- ss.lua
+++ ss.lua
@@ -1,3 +1,5 @@
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/HoafngKhooi/d-n-c-y-thu/refs/heads/bot-discord/ss.lua"))()
+
 -- [[ 1. KHỞI TẠO RAYFIELD UI ]]
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
