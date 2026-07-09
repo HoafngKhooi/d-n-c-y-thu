3480ee84525eeeeeb9dcab7157d505d656b5f29c
HoafngKhooi <hoafngkhooi@gmail.com>
2026-07-09 20:49:26 +0000

    feat: config capacitor, structure www folder and add build-apk script

--- Penguin/projects/IGelik/js/api/tiktok-config.js
+++
@@ -1,8 +0,0 @@
-// IGelik/js/api/tiktok-config.js
-const SOCIAL_CONFIG = {
-    tiktok: {
-        name: "TikTok",
-        url: "https://www.tiktok.com/embed", // Sử dụng endpoint nhúng để tối ưu hiển thị
-        icon: "📱"
-    }
-};
