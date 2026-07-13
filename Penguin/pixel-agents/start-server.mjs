5f8943c749832a415ee42d1fdd7f6d775a542ab0
HoafngKhooi <hoafngkhooi@gmail.com>
2026-07-13 20:43:26 +0000

    clear project

--- Penguin/pixel-agents/start-server.mjs
+++
@@ -1,4 +0,0 @@
-import { PixelAgentsServer } from './server/dist/server/src/server.js';
-
-const server = new PixelAgentsServer({ port: 3000, host: '0.0.0.0' });
-server.start().then(() => console.log('Server running on port 3000'));
